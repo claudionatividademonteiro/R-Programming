27c94ac4c88e9f332ad5b1957230867f8a1518bc
cladionatividademonteiro <claudio.natividade.monteiro@gmail.com>
2019-12-21 08:26:42 -0300

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+#loading required library
+library(dplyr)
+
+#getting file and loading into project directory
+file <- "GCD_project_dataset.zip"
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, file, method = "curl")
+unzip(file)
+
+
